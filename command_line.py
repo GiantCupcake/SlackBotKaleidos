32a64ad24f21bb0550cdc41696800fe7e260c92e
maxime.piergiov <maxime.piergio@he-arc.ch>
2016-06-01 10:38:56 +0200

    Fonction clean_words : retire les doublons Fonctions points_per_words : calcule le nb de pts que donnent chaque mots Fonctions count_points : calcule le nb de points par joueurs Ajoute des tests pour chaque fonction

--- command_line.py
+++ command_line.py
@@ -22,19 +22,54 @@ def state_validate_words(*words):
             if word not in dico:
                 dico.append(word)
     print(dico)
-    return dico
+    retire = input("eliminate wrong words by their numbers").split(" ")
+    for i in range(len(retire)):
+        retire[i] = int(retire[i])
+    print(retire)
+    eliminated = set()
+    for i in range(len(dico)):
+        if i in retire:
+            eliminated.add(dico[i])
+    print(eliminated)
+    return eliminated
 
-def state_count_points():
-    pass
+
+def clean_words(all_words, eliminated):
+    #Clean de chacun des all_words tous les mots contenus dans eliminated
+    validated_words = []
+    for groups in all_words:
+        remaining = []
+        for word in groups:
+            if word not in eliminated:
+                remaining.append(word)
+        validated_words.append(remaining)
+    return validated_words
+
+#Si on ne voit le mot qu'une fois, on lui assigne la valeur 4,sinon 1
+def points_per_words(valid_words):
+    valeur = dict()
+    for words in valid_words:
+        for word in words:
+            if word not in valeur:
+                valeur[word] = 4
+            else:
+                valeur[word] = 1
+    return valeur
+
+def count_points(group_words,value):
+    points = []
+    for group in group_words:
+        points.append(sum([value[word] for word in group]))
+    return points
 
 
 if __name__ == "__main__":
     #voic choice
     letter = chr(randint(0, 25) + 97)
-    words = [state_get_words(letter) for _ in range(3)]
-    print(words)
+    #all_words = [[],[],[]]
+    all_words = [state_get_words(letter) for _ in range(3)]
+    print(all_words)
     #pour le joueur 2
-    stateValidateWords[words[0],words[2]]
-    #for _ in range(3)
-    #        stateValidateWords(words)
-    #stateCountPoints()
+    eliminated = [state_validate_words(all_words[0],all_words[2]) for _ in range(3)]
+    validated_words = clean_words(all_words,eliminated)
+    word_value = points_per_words(validated_words)
--- test_command_line.py
+++ test_command_line.py
@@ -1,4 +1,22 @@
-from command_line import state_validate_words
+from command_line import *
 
-def test_state_validate_words():
-    assert [["a"], ["b"]] == state_validate_words(["b"], ["a"])
+#def test_state_validate_words():
+#    assert ["b","a"] == state_validate_words(["b"], ["a"])
+#    assert ["b","c","e"] == state_validate_words(["b","c"],["c"],["e"])
+
+values = {"a": 1,"ab": 4,"ac": 4,"ae": 1}
+words = [["a","ab","ae"],["a","ae","ac"]]
+
+def test_clean_words():
+    assert [["ab","ac"],["ad"]] == clean_words([["a","ab","ac"],["a","ad","ae"]],["a","ae"])
+
+
+def test_points_per_words():
+    b = points_per_words(words)
+    assert values.keys() == b.keys()
+    assert b["a"] == 1
+    assert b["ab"] == 4
+
+def test_count_points():
+    assert [6,6] == count_points(words,values)
+    assert [0,1] == count_points([[],["a"]],values)
